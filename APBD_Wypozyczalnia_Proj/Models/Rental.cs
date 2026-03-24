efb0576f4d0bffacd9d442f74eaf06bc2ad25163
MRDrewiena1 <kontad.dr@gmail.com>
2026-03-24 17:37:10 +0100

    + add model class of Rental.cs

---
+++ APBD_Wypozyczalnia_Proj/Models/Rental.cs
@@ -0,0 +1,27 @@
+namespace APBD_Wypozyczalnia_Proj.Models;
+
+public class Rental
+{
+    private static long Idcount { get; set; }
+    public long Id { get; set; }
+    public User User { get; set; }
+    public Equipment Equipment { get; set; }
+
+    public DateTime RentalDate { get; set; }
+    public int Days { get; set; }
+    public DateTime DueDate => RentalDate.AddDays(Days);
+
+    public DateTime ReturnDate { get; set; }
+
+    public Rental( User user, Equipment equipment, int days)
+    {
+        Idcount++;
+        this.Id = Idcount;
+        this.User = user;
+        this.Equipment = equipment;
+        this.Days = days;
+        this.RentalDate = DateTime.Now;
+    }
+
+
+}
